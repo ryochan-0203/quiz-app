50ebceffdba03183153ed5fab4b6cb568164d30b
ryosuke <ryosuke.9723@gmail.com>
2025-03-21 18:16:58 +0900

    routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from './contexts/AuthContext';
 import Login from './components/Login';
+import GenreSelection from './components/GenreSelection';
+import LevelSelection from './components/LevelSelection';
+import Quiz from './components/Quiz';
+import Result from './components/Result';
 import './App.css';
 
 function App() {
@@ -12,7 +16,23 @@ function App() {
       <Routes>
         <Route 
           path="/" 
-          element={currentUser ? <div>ログイン成功！</div> : <Login />} 
+          element={currentUser ? <Navigate to="/genres" /> : <Login />} 
+        />
+        <Route 
+          path="/genres" 
+          element={currentUser ? <GenreSelection /> : <Navigate to="/" />} 
+        />
+        <Route 
+          path="/genres/:genreId" 
+          element={currentUser ? <LevelSelection /> : <Navigate to="/" />} 
+        />
+        <Route 
+          path="/quiz/:genreId/:level" 
+          element={currentUser ? <Quiz /> : <Navigate to="/" />} 
+        />
+        <Route 
+          path="/result/:genreId/:level/:score/:questionIds" 
+          element={currentUser ? <Result /> : <Navigate to="/" />} 
         />
       </Routes>
     </div>
